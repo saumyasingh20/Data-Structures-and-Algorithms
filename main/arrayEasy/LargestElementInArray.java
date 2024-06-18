17bf1704706201cf5b4bb1df33affd6265a79bfd
Saumya Singh <saumyaaaa98@gmail.com>
2024-06-19 01:05:50 +0530

    updated
    DSA questions solved

---
+++ main/arrayEasy/LargestElementInArray.java
@@ -0,0 +1,17 @@
+package arrayEasy;
+
+public class LargestElementInArray {
+	
+	public int largest(int arr[], int n)
+    {
+        int max = Integer.MIN_VALUE;
+        for(int i =0 ;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+
+}
